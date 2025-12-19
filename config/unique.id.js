33954bdacfd5896d7e3a4289b6a280b482468a03
DravidTML <davanyiam@gmail.com>
2025-12-19 18:00:20 +0100

    Initial commit

--- config/unique.id.js
+++
@@ -1,7 +0,0 @@
-import crypto from "crypto";
-
-//configuration for generating  10 random digits
-export default function generateId() {
-    const id = crypto.randomInt(0, 10**10);
-    return id.toString().padStart(10, '0');
-}
